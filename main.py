47f89850b79007985f41179404b840eefd30f357
Mike Erdahl <mike.erdahl@gmail.com>
2016-04-07 02:13:33 +0000

    Quick-and-Dirty BeagleBone port
    Proof of concept port to Beagle Bone Black (other colors
    should be supported - verify IO).
    
    RPI GPIO library dependency was replaced with Adafruit-BBIO
    library.
    
    Efficiency improved by waiting for button press events instead
    of polling.
    
    Signed-off-by: Mike Erdahl <mike.erdahl@gmail.com>

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import os
 import random
 import time
-import RPi.GPIO as GPIO
+import Adafruit_BBIO.GPIO as GPIO
 import alsaaudio
 import wave
 import random
@@ -11,11 +11,20 @@ from creds import *
 import requests
 import json
 import re
+import signal
+import sys
 from memcache import Client
 
+# Catch ctrl-c and clean up GPIO
+def signal_handler(signal, frame):
+	GPIO.cleanup()
+	sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
 #Settings
-button = 18 #GPIO Pin with button connected
-lights = [24, 25] # GPIO Pins with LED's conneted
+button = "GPIO0_7" #GPIO Pin with button connected
+led1 = "GPIO0_30"   # GPIO Pins with LED's conneted
+led2 = "GPIO1_28"
 device = "plughw:1" # Name of your microphone/soundcard in arecord -L
 
 #Setup
@@ -23,8 +32,6 @@ recorded = False
 servers = ["127.0.0.1:11211"]
 mc = Client(servers, debug=1)
 path = os.path.realpath(__file__).rstrip(os.path.basename(__file__))
-
-
 
 def internet_on():
     print "Checking Internet Connection"
@@ -54,7 +61,7 @@ def gettoken():
 		
 
 def alexa():
-	GPIO.output(24, GPIO.HIGH)
+	GPIO.output(led1, GPIO.HIGH)
 	url = 'https://access-alexa-na.amazon.com/v1/avs/speechrecognizer/recognize'
 	headers = {'Authorization' : 'Bearer %s' % gettoken()}
 	d = {
@@ -93,23 +100,31 @@ def alexa():
 				audio = d.split('\r\n\r\n')[1].rstrip('--')
 		with open(path+"response.mp3", 'wb') as f:
 			f.write(audio)
-		GPIO.output(25, GPIO.LOW)
+		GPIO.output(led2, GPIO.LOW)
 		os.system('mpg123 -q {}1sec.mp3 {}response.mp3'.format(path, path))
-		GPIO.output(24, GPIO.LOW)
+		GPIO.output(led1, GPIO.LOW)
 	else:
-		GPIO.output(lights, GPIO.LOW)
+		GPIO.output(led1, GPIO.LOW)
+		GPIO.output(led2, GPIO.LOW)
 		for x in range(0, 3):
 			time.sleep(.2)
-			GPIO.output(25, GPIO.HIGH)
+			GPIO.output(led2, GPIO.HIGH)
 			time.sleep(.2)
-			GPIO.output(lights, GPIO.LOW)
+			GPIO.output(led2, GPIO.LOW)
 		
 
 
 
 def start():
+	global recorded
 	last = GPIO.input(button)
 	while True:
+		# wait for an edge event
+		if recorded == False:
+			print "Waiting for button press"
+			GPIO.wait_for_edge(button,GPIO.BOTH)
+		
+		print "pressed!"	
 		val = GPIO.input(button)
 		if val != last:
 			last = val
@@ -118,9 +133,10 @@ def start():
 				rf.write(audio)
 				rf.close()
 				inp = None
+				recorded = False
 				alexa()
 			elif val == 0:
-				GPIO.output(25, GPIO.HIGH)
+				GPIO.output(led2, GPIO.HIGH)
 				inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, device)
 				inp.setchannels(1)
 				inp.setrate(16000)
@@ -138,19 +154,21 @@ def start():
 	
 
 if __name__ == "__main__":
-	GPIO.setwarnings(False)
+	#GPIO.setwarnings(False)
 	GPIO.cleanup()
-	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(lights, GPIO.OUT)
-	GPIO.output(lights, GPIO.LOW)
+	GPIO.setup(led1, GPIO.OUT)
+	GPIO.output(led1, GPIO.LOW)
+	GPIO.setup(led2, GPIO.OUT)
+	GPIO.output(led2, GPIO.LOW)
+	GPIO.add_event_detect(button,GPIO.BOTH)
 	while internet_on() == False:
 		print "."
 	token = gettoken()
 	os.system('mpg123 -q {}1sec.mp3 {}hello.mp3'.format(path, path))
 	for x in range(0, 3):
 		time.sleep(.1)
-		GPIO.output(24, GPIO.HIGH)
+		GPIO.output(led1, GPIO.HIGH)
 		time.sleep(.1)
-		GPIO.output(24, GPIO.LOW)
+		GPIO.output(led1, GPIO.LOW)
 	start()
