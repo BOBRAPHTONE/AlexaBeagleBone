6d9b45a738bad6d9c7874f47a19fe163e5454c80
merdahl <mike.erdahl@gmail.com>
2016-04-20 15:50:23 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ wget --output-document vlc.py "http://git.videolan.org/?p=vlc/bindings/python.gi
 apt-get update
 apt-get install libasound2-dev memcached python-pip python-alsaaudio vlc -y
 pip install -r requirements.txt
-cp initd_alexa.sh /etc/init.d/AlexaPi
-update-rc.d AlexaPi defaults
+cp initd_alexa.sh /etc/init.d/AlexaBeagleBone
+update-rc.d AlexaBeagleBone defaults
 touch /var/log/alexa.log
 
 echo "Enter your ProductID:"
